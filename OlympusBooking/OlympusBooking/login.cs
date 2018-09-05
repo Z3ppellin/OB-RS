ca21ece4c8cc26425e4737dca2dc0623e5219513
jackattack691 <42969124+jackattack691@users.noreply.github.com>
2018-09-05 12:14:14 +0200

    fake

--- OlympusBooking/OlympusBooking/login.cs
+++ OlympusBooking/OlympusBooking/login.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.OleDb;
+using System.Data.SqlClient;
 
 namespace OlympusBooking
 {
@@ -16,5 +18,52 @@ namespace OlympusBooking
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {                     
+            OleDbConnection conn = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source = database.accdb");
+           
+            OleDbCommand cmd = new OleDbCommand();
+            int i =0;
+            try
+            {
+                if ((Username.Text == string.Empty) || tbPassword.Text == string.Empty)
+                {
+                    MessageBox.Show("Please enter your username and password");
+                }
+
+                cmd = new OleDbCommand("SELECT count(*) FROM tblUser WHERE username = '" + Username.Text + "' AND password = '" + tbPassword.Text + "'", conn);
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                    i = (int)cmd.ExecuteScalar();
+
+                }
+                conn.Close();
+                if (i > 0)
+                {
+                    this.Close();
+                    MessageBox.Show("Yass");
+                }
+                else
+                {
+                    MessageBox.Show("Invalid login!");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+           
+                
+        }
+
     }
 }
