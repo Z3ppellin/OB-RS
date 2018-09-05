e3b744067a51f296a53a5454a73113f87decf0ef
jackattack691 <42969124+jackattack691@users.noreply.github.com>
2018-09-05 15:57:29 +0200

    forms

--- OlympusBooking/OlympusBooking/frmCheckIn.cs
+++ OlympusBooking/OlympusBooking/frmCheckIn.cs
@@ -17,5 +17,45 @@ namespace OlympusBooking
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCheckIn_Click(object sender, EventArgs e)
+        {
+            //Declaring variables used in the check form
+            string guestName;
+            string roomNum;
+            string roomType;
+            string roomRate;
+            string checkInDate;
+            string numOfDays;
+            string numAdults;
+            string numChildren;
+            string subTotal;
+            string totalBalance;
+
+            //Initialising variables to given values
+            guestName = tbGuestName.Text;
+            roomNum = tbRoomNumber.Text;
+            roomType = tbRoomType.Text;
+            roomRate = tbRoomRate.Text;
+            checkInDate = DateTime.Now.ToString();
+
+            numOfDays = tbNoOfDays.Text;
+            numAdults = cbAdults.SelectedValue.ToString();
+            numChildren = cbChildren.SelectedValue.ToString();
+            subTotal = tbTotal.Text.ToString();
+            totalBalance = tbBalance.Text.ToString();
+
+        }
+
+
+        private void frmCheckIn_Load(object sender, EventArgs e)
+        {
+            tbCheckInDate.Text = DateTime.Now.ToString();
+        }
     }
 }
