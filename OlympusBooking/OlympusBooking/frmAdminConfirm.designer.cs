4e2363da33b1543874183e0e56949ee83108e407
Jacques Dippenaar <jaakdippenaar@gmail.com>
2018-10-24 14:23:57 +0200

    Icons frmForgotPassword frmAdminCpnfirm

---
+++ OlympusBooking/OlympusBooking/frmAdminConfirm.designer.cs
@@ -0,0 +1,101 @@
+﻿namespace OlympusBooking
+{
+	partial class frmAdminConfirm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.lblAdminPassword = new System.Windows.Forms.Label();
+			this.btnConfirm = new System.Windows.Forms.Button();
+			this.btnCancel = new System.Windows.Forms.Button();
+			this.tbAdminPassword = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// lblAdminPassword
+			// 
+			this.lblAdminPassword.AutoSize = true;
+			this.lblAdminPassword.Location = new System.Drawing.Point(68, 45);
+			this.lblAdminPassword.Name = "lblAdminPassword";
+			this.lblAdminPassword.Size = new System.Drawing.Size(85, 13);
+			this.lblAdminPassword.TabIndex = 1;
+			this.lblAdminPassword.Text = "Admin Password";
+			// 
+			// btnConfirm
+			// 
+			this.btnConfirm.Location = new System.Drawing.Point(71, 87);
+			this.btnConfirm.Name = "btnConfirm";
+			this.btnConfirm.Size = new System.Drawing.Size(75, 23);
+			this.btnConfirm.TabIndex = 2;
+			this.btnConfirm.Text = "Confirm";
+			this.btnConfirm.UseVisualStyleBackColor = true;
+			this.btnConfirm.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// btnCancel
+			// 
+			this.btnCancel.Location = new System.Drawing.Point(152, 87);
+			this.btnCancel.Name = "btnCancel";
+			this.btnCancel.Size = new System.Drawing.Size(75, 23);
+			this.btnCancel.TabIndex = 3;
+			this.btnCancel.Text = "Cancel";
+			this.btnCancel.UseVisualStyleBackColor = true;
+			this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+			// 
+			// tbAdminPassword
+			// 
+			this.tbAdminPassword.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.tbAdminPassword.Location = new System.Drawing.Point(71, 61);
+			this.tbAdminPassword.Name = "tbAdminPassword";
+			this.tbAdminPassword.PasswordChar = '•';
+			this.tbAdminPassword.Size = new System.Drawing.Size(156, 20);
+			this.tbAdminPassword.TabIndex = 5;
+			// 
+			// frmAdminConfirm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+			this.ClientSize = new System.Drawing.Size(312, 160);
+			this.Controls.Add(this.tbAdminPassword);
+			this.Controls.Add(this.btnCancel);
+			this.Controls.Add(this.btnConfirm);
+			this.Controls.Add(this.lblAdminPassword);
+			this.Name = "frmAdminConfirm";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Admin Confirm";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+		private System.Windows.Forms.Label lblAdminPassword;
+		private System.Windows.Forms.Button btnConfirm;
+		private System.Windows.Forms.Button btnCancel;
+		private System.Windows.Forms.TextBox tbAdminPassword;
+	}
+}
---
+++ OlympusBooking/OlympusBooking/frmForgotPassword.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace OlympusBooking
+{
+	public partial class frmForgotPassword : Form
+	{
+		public frmForgotPassword()
+		{
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{										
+			string newPassword;
+			string userName;
+
+			//Opening connection to database
+			OleDbCommand cmd = new OleDbCommand();
+			OleDbConnection myCon = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\Users\User\Desktop\Olympus Hotel\OlympusBooking\OlympusBooking\App_Data\database.accdb");
+
+			//Initialising variables
+			newPassword = tbPassword.Text;
+			userName = tbUserName.Text;
+			if (tbPassword.Text != tbConfirmPassword.Text)
+			{
+				epPassword.SetError(this.tbConfirmPassword, "Passwords do not match");
+			}
+			else
+			{
+				cmd.CommandType = CommandType.Text;
+				cmd.CommandText = "UPDATE [User] SET [Password] = @newPassword WHERE [Username] = @userName";
+
+				cmd.Parameters.AddWithValue("@newPassword", newPassword);
+				cmd.Parameters.AddWithValue("@userName", userName);
+
+				cmd.Connection = myCon;
+				myCon.Open();
+				cmd.ExecuteNonQuery();
+				System.Windows.Forms.MessageBox.Show("Password has been reset!", "Caption", MessageBoxButtons.OKCancel, MessageBoxIcon.Information);
+				myCon.Close();
+								
+				Application.Restart();
+				
+			}
+		}
+
+		private void btnCancel_Click(object sender, EventArgs e)
+		{
+			this.Close();
+		}
+	}
+}
